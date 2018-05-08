6e1490e5207980b2244dbf8905654e3ae99d733e
Angelina Wong <angelina.w25@gmail.com>
2018-05-08 19:55:25 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   empty_array = 0
 
   while empty_array < languages.length
-    yield(languages[empty_array.upcase])
+    yield(languages.upcase)
 
     empty_array += 1
   end
